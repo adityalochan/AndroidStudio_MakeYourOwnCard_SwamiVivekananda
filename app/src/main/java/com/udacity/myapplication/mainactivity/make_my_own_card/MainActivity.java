e5ea68b9862ad78f3b9fe881172d35668c3a4341
Sharma <adityalochan@usf.edu>
2018-02-03 23:18:55 -0500

    Creating your own card on Android Studio

---
+++ app/src/main/java/com/udacity/myapplication/mainactivity/make_my_own_card/MainActivity.java
@@ -0,0 +1,13 @@
+package com.udacity.myapplication.mainactivity.make_my_own_card;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
